f9d51a29741ab3f1f16622f37854c03e434a70a2
dayaneoc <dayaneoliveira.eng@gmail.com>
2026-01-14 19:27:11 -0300

    trabalhando

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -3,9 +3,12 @@ function soma() {
     var num2 = parseFloat(document.getElementById ("n2").value)
     var resp = document.getElementById ("resp_soma")
 
+    if (isNaN(num1)|| isNaN(num2)) {
+        alert ("Digite dois números válidos")
+        return
+    }
     //console.log (typeof num1, typeof num2)
     resp.textContent = num1 + num2
-    
 }
 
 function subtrai () {
@@ -13,8 +16,11 @@ function subtrai () {
     var num2 = parseFloat(document.getElementById ("n4").value)
     var resp = document.getElementById ("resp_subtrai")
 
+    if (isNaN(num1)|| isNaN(num2)) {
+        alert ("Digite dois números válidos")
+        return
+    }
     resp.innerHTML = num1 - num2
-
 }
 
 function mult () {
@@ -22,18 +28,29 @@ function mult () {
     var num2 = parseFloat(document.getElementById ("n6").value)
     var resp = document.getElementById ("resp_mult")
 
+    if (isNaN(num1)|| isNaN(num2)) {
+        alert ("Digite dois números válidos")
+        return
+    }
     resp.innerHTML = num1 * num2
-
 }
 
 function dividir () {
     var num1 = parseFloat(document.getElementById ("n7").value)
     var num2 = parseFloat(document.getElementById ("n8").value)
-    var resp = document.getElementById ("resp_div")
+   // var resp = document.getElementById ("resp_div")
 
-    
-    
-    resp.innerHTML = num1 / num2
+    if (num2 === 0) {
+        document.getElementById("resp_div").innerHTML = "Não há divisão por 0, digite outro número!!!"
+        retturn
+    }
+
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById("resp_div").innerHTML = "Digite dois números válidos"
+        return
+    }
+
+    document.getElementById ("resp_div").innerHTML = num1 / num2
 
     //condicional, que se o primeiro num digitado for === 0,
     // textContent = "Não existe divisão por 0"
